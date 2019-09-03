f099ddd36d48adc6898f29e5012c6331126c11bf
khalidhmd <khalidhmd@outlook.com>
2019-09-03 17:31:10 +0200

    add link click and scroll functionality

--- js/app.js
+++ js/app.js
@@ -17,14 +17,14 @@
  * Define Global Variables
  *
  */
-
+const navBarList = document.getElementById('navbar__list');
+const sectionList = document.querySelectorAll('[data-nav]')
 
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
-
 
 
 /**
@@ -34,13 +34,42 @@
  */
 
 // build the nav
-
+const populateLinks = () => {
+  const fragment = document.createDocumentFragment();
+  sectionList.forEach(section => {
+    const li = document.createElement('li');
+    li.innerHTML = `<a href="#" data-scroll="${section.getAttribute('id')}" class="menu__link">${section.dataset.nav}</a>`
+    fragment.appendChild(li);
+  })
+  navBarList.appendChild(fragment)
+}
 
 // Add class 'active' to section when near top of viewport
-
+const setActiveSection = () => {
+  for (let i = 0; i < sectionList.length; i++) {
+    const boundary = sectionList[i].getBoundingClientRect();
+    if ((boundary.top < (window.innerHeight / 3)) && (boundary.top > 0 - (window.innerHeight / 3))) {
+      sectionList[i].classList.toggle('your-active-class', true)
+      continue
+    }
+    sectionList[i].classList.toggle('your-active-class', false)
+  }
+}
 
 // Scroll to anchor ID using scrollTO event
-
+const scrollSection = (e) => {
+  e.preventDefault()
+  for (let i = 0; i < sectionList.length; i++) {
+    if (sectionList[i].getAttribute('id') == e.target.dataset.scroll) {
+      const ele = document.getElementById(sectionList[i].getAttribute('id'))
+      console.log(sectionList[i].getBoundingClientRect().top)
+      console.log(sectionList[i].getBoundingClientRect().left)
+      window.scroll(sectionList[i].getBoundingClientRect().left, sectionList[i].getBoundingClientRect().top)
+      // console.log(sectionList[i])
+      // console.log(e.target.dataset.scroll)
+    }
+  }
+}
 
 /**
  * End Main Functions
@@ -49,7 +78,10 @@
  */
 
 // Build menu 
+populateLinks()
 
 // Scroll to section on link click
+navBarList.addEventListener('click', scrollSection)
 
 // Set sections as active
+document.addEventListener('scroll', setActiveSection)
